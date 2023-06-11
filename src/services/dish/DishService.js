45bbd2be2a6cebce3e3613a3c2980d07ae982629
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-06-11 09:09:39 -0300

    feat: Added multer service on dish to upload and delete files

--- src/services/dish/DishService.js
+++ src/services/dish/DishService.js
@@ -42,8 +42,9 @@ export class DishService {
         return dishWithIngredients;
     }
 
-    async create({ name, description, picture, avatar, price, category_id, ingredients }) {
+    async create({ name, description, picture, price, category_id, ingredients }) {
         const nameInUse = await this.repository.checkNameInUse({ name });
+        const diskStorage = new DiskStorage();
 
         if (nameInUse) throw new AppError('Nome do prato já cadastrado.', 409);
 
@@ -52,7 +53,9 @@ export class DishService {
         if (ingredients.length == 0) {
             throw new AppError('Informe os ingredientes do prato.');
         }
-
+        if (picture) {
+            await diskStorage.saveFile(picture);
+        }
         // check if is string typeof myVar === 'string' || myVar instanceof String
 
         const updated_at = dayjs().format();
@@ -80,14 +83,20 @@ export class DishService {
     }
     async update({ id, name, description, picture, price, category_id, ingredients }) {
         const dish = await this.repository.findById(id);
-
         const nameInUse = await this.repository.checkNameInUse({ name, id });
+        const diskStorage = new DiskStorage();
 
         if (nameInUse) throw new AppError('Nome do prato já cadastrado.', 409);
 
+        const ingredientsInsert = ingredients.map(ingredient => {
+            return {
+                ...ingredient,
+                dish_id: dish.id,
+            };
+        });
+
         dish.name = name ?? dish.name;
         dish.description = description ?? dish.description;
-        dish.picture = picture ?? dish.picture;
         dish.price = price ?? dish.price;
         dish.category_id = category_id ?? dish.category_id;
 
@@ -96,23 +105,34 @@ export class DishService {
         if (ingredients.length == 0) {
             throw new AppError('Informe os ingredientes do prato.');
         }
-        const ingredientsInsert = ingredients.map(ingredient => {
-            return {
-                ...ingredient,
-                dish_id: dish.id,
-            };
-        });
+
+        const hasChangedPicture = picture && dish.picture;
+
+        if (hasChangedPicture) {
+            await diskStorage.deleteFile(dish.picture);
+            await diskStorage.saveFile(picture);
+        } else if (picture) {
+            await diskStorage.saveFile(picture);
+        }
+
+        dish.picture = picture ?? dish.picture;
 
         dish.updated_at = dayjs().format();
 
         await this.repository.update(dish);
-
         await this.repository.removeIngredientsByDish(dish.id);
         const ingredients_ = await this.repository.insertIngredientsByDish(ingredientsInsert);
+
         return ingredients_;
     }
     async delete(id) {
-        const dishes = await this.repository.delete(id);
-        return dishes;
+        const diskStorage = new DiskStorage();
+        const dish = await this.repository.delete(id);
+
+        if (dish.picture) {
+            await diskStorage.deleteFile(dish.picture);
+        }
+
+        return dish;
     }
 }
