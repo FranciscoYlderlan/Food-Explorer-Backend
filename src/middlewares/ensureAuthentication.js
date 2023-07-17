cb5ab1a14c271b1474962e4226abd7a6655d861b
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-07-17 12:14:03 -0300

    fix: remove query into middleware authentication

--- src/middlewares/ensureAuthentication.js
+++ src/middlewares/ensureAuthentication.js
@@ -1,7 +1,6 @@
 import jwt from 'jsonwebtoken';
 import authConfigs from '../configs/auth.js';
 import { AppError } from '../utils/AppError.js';
-import { UserRepository } from '../repositories/user/Repository.js';
 
 export function ensureAuthentication(request, response, next) {
     const authHeader = request.headers.authorization;
@@ -11,19 +10,10 @@ export function ensureAuthentication(request, response, next) {
     const [, token] = authHeader.split(' ');
 
     const { verify } = jwt;
-    const userRepository = new UserRepository();
 
     try {
         const { secret } = authConfigs.jwt;
         const { sub: user_id, isAdmin } = verify(token, secret);
-
-        const user = userRepository.findById(user_id);
-        if (user) {
-            const userIsAdmin = user.profile_id === 1 && Boolean(isAdmin);
-            isAdmin = userIsAdmin;
-        } else {
-            isAdmin = false;
-        }
 
         request.user = {
             id: Number(user_id),
